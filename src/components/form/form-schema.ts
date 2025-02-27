c84a277ebe94f8978bc4d4bd1714fc8946adea48
QueencessM <munsayacqueencess.work@gmail.com>
2025-02-27 20:29:32 +0800

    edited error message

--- src/components/form/form-schema.ts
+++ src/components/form/form-schema.ts
@@ -1,7 +1,7 @@
 import { z } from 'zod';
 
 export const formSchema = z.object({
-    amount: z.coerce.number().nonnegative('Amount must be a positive number.'),
+    amount: z.coerce.number({ message: 'Amount should be a number.' }).nonnegative('Amount must be a positive number.'),
     base: z.string({ message: 'Base currency has invalid input.' }).length(3, 'No Base Currency selected.'),
     target: z.string({ message: 'Target currency has invalid input.' }).length(3, 'No Target Currency selected.'),
 });
